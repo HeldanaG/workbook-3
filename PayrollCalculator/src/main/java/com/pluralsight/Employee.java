65779ae7c7a6ff549d69e563ac52dc0715025a59
HeldanaG <heldanagmariam@gmail.com>
2025-04-23 11:19:43 -0500

    Completed Mod1 - Exercise 3 - Pay Roll Calculator

---
+++ PayrollCalculator/src/main/java/com/pluralsight/Employee.java
@@ -0,0 +1,52 @@
+package com.pluralsight;
+
+public class Employee {
+
+    private int employeeId=0;
+    private String name="";
+    private double hoursWorked=0;
+    private double payRate=0;
+
+    public Employee(int employeeId, String name, double hoursWorked, double payRate) {
+        this.employeeId = employeeId;
+        this.name = name;
+        this.hoursWorked = hoursWorked;
+        this.payRate = payRate;
+    }
+
+    public double getGrossPay(){
+        return hoursWorked * payRate;
+    }
+
+    public int getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(int employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public double getPayRate() {
+        return payRate;
+    }
+
+    public void setPayRate(double payRate) {
+        this.payRate = payRate;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getHoursWorked() {
+        return hoursWorked;
+    }
+
+    public void setHoursWorked(double hoursWorked) {
+        this.hoursWorked = hoursWorked;
+    }
+}
---
+++ PayrollCalculator/src/main/java/com/pluralsight/PayrollCalculator.java
@@ -0,0 +1,52 @@
+package com.pluralsight;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+
+public class PayrollCalculator {
+
+    public static void main(String[] args) {
+
+        try {
+            // File name
+            String fileName = "employees.csv";
+
+            // Open the file
+            FileReader theFile = new FileReader("src/main/resources/" + fileName);
+            BufferedReader ourBufferedReader = new BufferedReader(theFile);
+
+            String theLine;
+
+            // Skip the header line
+            ourBufferedReader.readLine();
+
+            // Read each data line
+            while ((theLine = ourBufferedReader.readLine()) != null) {
+                // Split line by '|'
+                String[] employeeDetails = theLine.split("\\|");
+
+                // Create Employee object with corrected field order
+                Employee employeeInfo = new Employee(
+                        Integer.parseInt(employeeDetails[0]),    // id
+                        employeeDetails[1],                     // name
+                        Double.parseDouble(employeeDetails[2]), // hours-worked
+                        Double.parseDouble(employeeDetails[3])  // pay-rate
+                );
+
+                // Print employee payroll info
+                System.out.printf(
+                        "Employee ID: %d | Name: %s | Hours Worked: %.2f | Pay Rate: $%.2f | Gross Pay: $%.2f\n",
+                        employeeInfo.getEmployeeId(),
+                        employeeInfo.getName(),
+                        employeeInfo.getHoursWorked(),
+                        employeeInfo.getPayRate(),
+                        employeeInfo.getGrossPay());
+            }
+            // Close the file reader
+            ourBufferedReader.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
