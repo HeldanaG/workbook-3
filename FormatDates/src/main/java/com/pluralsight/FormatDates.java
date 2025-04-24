af940f5a4a7872c51a427ed0cdf3e46a3d9f5d38
HeldanaG <heldanagmariam@gmail.com>
2025-04-24 11:05:32 -0500

    Completed Mod2 - Exercise 2 - Format Dates

--- FormatDates/src/main/java/com/pluralsight/FormatDates.java
+++ FormatDates/src/main/java/com/pluralsight/FormatDates.java
@@ -2,30 +2,27 @@ package com.pluralsight;
 
 import java.time.LocalDateTime;
 import java.time.LocalTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Scanner;
 
 public class FormatDates {
     public static void main(String[] args) {
 
-        LocalDateTime currentDateAndTime = LocalDateTime.now();
+        // Get the current local date and time
+        LocalDateTime localNow = LocalDateTime.now();
 
-        DateTimeFormatter formatter1 = DateTimeFormatter.ofPattern("MM/dd/yyyy");
-        System.out.println("Format 1: " + currentDateAndTime.format(formatter1));
+        // Convert the local date and time to GMT timezone
+        ZonedDateTime gmtNow = localNow.atZone(ZoneId.systemDefault()).withZoneSameInstant(ZoneId.of("GMT"));
 
-        DateTimeFormatter formatter2 = DateTimeFormatter.ofPattern("yyyy-MM-dd");
-        System.out.println("Format 2: " + currentDateAndTime.format(formatter2));
-
-        DateTimeFormatter formatter3 = DateTimeFormatter.ofPattern("MMM d, yyyy ");
-        System.out.println("Format 3: " + currentDateAndTime.format(formatter3));
-
-        DateTimeFormatter formatter4 = DateTimeFormatter.ofPattern("EEEE, MMM d, yyyy  HH:mm");
-        System.out.println("Format 4: " + currentDateAndTime.format(formatter4));
-
-        DateTimeFormatter formatter5 = DateTimeFormatter.ofPattern("HH:mm");
-        DateTimeFormatter formatter6 = DateTimeFormatter.ofPattern("d-MMM-yyyy");
-
-        System.out.println("Format 5: " + currentDateAndTime.format(formatter5) + " on " + currentDateAndTime.format(formatter6));
+        // Print the date
+        System.out.println("Format 1: " + localNow.format(DateTimeFormatter.ofPattern("MM/dd/yyyy")));
+        System.out.println("Format 2: " + localNow.format(DateTimeFormatter.ofPattern("yyyy-MM-dd")));
+        System.out.println("Format 3: " + localNow.format(DateTimeFormatter.ofPattern("MMM d, yyyy ")));
+        System.out.println("Format 4: " + localNow.format(DateTimeFormatter.ofPattern("EEEE, MMM d, yyyy  HH:mm")));
+        System.out.println("Format 5: " + localNow.format(DateTimeFormatter.ofPattern("HH:mm")) + " on "
+                + localNow.format(DateTimeFormatter.ofPattern("d-MMM-yyyy")));
 
 
 
